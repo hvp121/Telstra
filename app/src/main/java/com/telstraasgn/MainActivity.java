06810aa43130fe2055b0bfb56fdebe3488f7c50b
HA20058573 <harshal.patil7@wipro.com>
2019-01-30 15:57:46 +0530

    Test Commit 1

--- app/src/main/java/com/telstraasgn/MainActivity.java
+++ app/src/main/java/com/telstraasgn/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    int abc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
