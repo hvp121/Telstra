449506f3fabafacb9b32abb6d67ddad65a142fdd
ja20049996 <developer.jayesh@gmail.com>
2019-02-01 16:24:30 +0530

    Adding Test Cases

--- app/src/test/java/com/telstraasgn/CountryUnitTest.java
+++ app/src/test/java/com/telstraasgn/CountryUnitTest.java
@@ -9,7 +9,8 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class CountryUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
